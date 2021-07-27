d1e2e7e228155b80058d128c484d64f41225ad51
EpicMario71 <56398081+epicmario7133@users.noreply.github.com>
2021-07-27 13:32:11 +0200

    Update main.py
    added more format

--- main.py
+++ main.py
@@ -49,4 +49,93 @@ async def on_message(message):
                 json.dump(data, outfile)
             await message.channel.send('Now the bot is ready')
             
+            
+           
+
+    #Convert .mov
+
+    if str(message.attachments) == "[]": # Checks if there is an attachment on the message
+        return
+
+    if data["work"] == "1": #Check if bot is working
+        await message.channel.send('Bot is working please wait')
+        pass
+
+    else: # If there is it gets the filename from message.attachments
+
+        split_v1 = str(message.attachments).split("filename='")[1]
+        filename = str(split_v1).split("' ")[0]
+        if filename.endswith(".mov"): # Checks if it is a .mov file
+            data["work"] = "1"
+            with open('data.json', 'w') as outfile:
+                json.dump(data, outfile)
+            await message.attachments[0].save(fp="mov/{}".format("video.mov")) #save flie
+            await message.channel.send('I\'m converting the video give me 10 seconds') 
+            os.system("ffmpeg -i /home/ubuntu/bot-mp3/mov/video.mov -y /home/ubuntu/bot-mp3/mp3/audio.mp3") #convert whit ffmpeg
+            time.sleep(10) #Give time for bot to convert, set 20 if your pc is a potato
+            await message.channel.send(file=discord.File("/home/ubuntu/bot-mp3/mp3/audio.mp3")) #Send file
+            time.sleep(3) #Wait bot upload file
+            data["work"] = "0" #set bot to ready
+            with open('data.json', 'w') as outfile:
+                json.dump(data, outfile)
+            await message.channel.send('Now the bot is ready')
+            
+    #for avi video
+
+    if str(message.attachments) == "[]": # Checks if there is an attachment on the message
+        return
+
+    if data["work"] == "1": #Check if bot is working
+        await message.channel.send('Bot is working please wait')
+        pass
+
+    else: # If there is it gets the filename from message.attachments
+
+        split_v1 = str(message.attachments).split("filename='")[1]
+        filename = str(split_v1).split("' ")[0]
+        if filename.endswith(".avi"): # Checks if it is a .avi file
+            data["work"] = "1"
+            with open('data.json', 'w') as outfile:
+                json.dump(data, outfile)
+            await message.attachments[0].save(fp="avi/{}".format("video.avi")) #save flie
+            await message.channel.send('I\'m converting the video give me 10 seconds') 
+            os.system("ffmpeg -i /home/ubuntu/bot-mp3/avi/video.avi -y /home/ubuntu/bot-mp3/mp3/audio.mp3") #convert whit ffmpeg
+            time.sleep(10) #Give time for bot to convert, set 20 if your pc is a potato
+            await message.channel.send(file=discord.File("/home/ubuntu/bot-mp3/mp3/audio.mp3")) #Send file
+            time.sleep(3) #Wait bot upload file
+            data["work"] = "0" #set bot to ready
+            with open('data.json', 'w') as outfile:
+                json.dump(data, outfile)
+            await message.channel.send('Now the bot is ready')
+
+
+
+    #for webm video
+
+    if str(message.attachments) == "[]": # Checks if there is an attachment on the message
+        return
+
+    if data["work"] == "1": #Check if bot is working
+        await message.channel.send('Bot is working please wait')
+        pass
+
+    else: # If there is it gets the filename from message.attachments
+
+        split_v1 = str(message.attachments).split("filename='")[1]
+        filename = str(split_v1).split("' ")[0]
+        if filename.endswith(".webm"): # Checks if it is a .avi file
+            data["work"] = "1"
+            with open('data.json', 'w') as outfile:
+                json.dump(data, outfile)
+            await message.attachments[0].save(fp="webm/{}".format("video.webm")) #save flie
+            await message.channel.send('I\'m converting the video give me 10 seconds') 
+            os.system("ffmpeg -i /home/ubuntu/bot-mp3/webm/video.webm -y /home/ubuntu/bot-mp3/mp3/audio.mp3") #convert whit ffmpeg
+            time.sleep(10) #Give time for bot to convert, set 20 if your pc is a potato
+            await message.channel.send(file=discord.File("/home/ubuntu/bot-mp3/mp3/audio.mp3")) #Send file
+            time.sleep(3) #Wait bot upload file
+            data["work"] = "0" #set bot to ready
+            with open('data.json', 'w') as outfile:
+                json.dump(data, outfile)
+            await message.channel.send('Now the bot is ready')
+            
 client.run(TOKEN)
